92f74daf8c43c4e0109850db9af75251417ef2a2
MrSamix <alekssen2008@gmail.com>
2026-07-19 17:14:40 +0300

    add QueryRTK

---
+++ store/index.ts
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { type TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const store = configureStore({
+    reducer: {
+        // [apiAccount.reducerPath]: apiAccount.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware(), // getDefaultMiddleware().concat(apiAccount.middleware)
+});
+
+// Типи, які знаходяться у Redux
+export type RootState = ReturnType<typeof store.getState>;
+// Метод, який дає команди для reducer - залогінь, вийди із акаунта
+export type AppDispatch = typeof store.dispatch;
+
+// виклик різних методів із глобального стора
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+// Отримуємо дані із глобального стора на основі типів, які там є
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
